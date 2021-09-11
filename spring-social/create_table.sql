13c36c23d85275055a75ea18bc6d3e2de2f84ef6
jhnguye4 <jhnguye4@ncsu.edu>
2021-09-11 14:15:36 -0400

    added inventory table to sql

--- spring-social/create_table.sql
+++ spring-social/create_table.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE inventory_tracker;
 USE inventory_tracker;
 
 CREATE TABLE `users3` (
@@ -11,3 +12,15 @@ CREATE TABLE `users3` (
   UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `inventory` (
+  `restaurantName` varchar(255) NOT NULL,
+  `restuarantID` varchar(45) NOT NULL,
+  `itemID` varchar(45) NOT NULL,
+  `itemName` varchar(255) NOT NULL,
+  `batchID` varchar(45) NOT NULL,
+  `batchQty` int NOT NULL,
+  `costPerItem` double NOT NULL,
+  `dateBought` datetime NOT NULL,
+  `dateExpired` datetime NOT NULL,
+  PRIMARY KEY (`restuarantID`,`itemID`,`batchID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
