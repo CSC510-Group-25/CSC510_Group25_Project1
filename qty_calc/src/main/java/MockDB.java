d3894c1033776bb3974cb37287f95958487f4f9b
Leila Moran <89357283+snapcat@users.noreply.github.com>
2021-09-26 18:16:05 -0400

    Update and rename qty_calc/src/main/resources/MockDB.java to qty_calc/src/main/java/MockDB.java

--- qty_calc/src/main/java/MockDB.java
+++ qty_calc/src/main/java/MockDB.java
@@ -1,4 +1,4 @@
-package com.qty_calc;
+//package com.qty_calc;
 import com.github.cliftonlabs.json_simple.JsonArray;
 import com.github.cliftonlabs.json_simple.JsonException;
 import com.github.cliftonlabs.json_simple.JsonObject;
