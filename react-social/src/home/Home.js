0e3fbd5068655861218df45d84cc2a9ce29d6e7a
Emily Tracey <etracey@ncsu.edu>
2021-09-27 18:57:17 -0400

    Changed name across app

--- react-social/src/home/Home.js
+++ react-social/src/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css';
+import Typography from "@material-ui/core/Typography";
 
 class Home extends Component {
     render() {
@@ -21,7 +22,21 @@ class Home extends Component {
                             <div className="graf-circle"></div>
                         </div>
                     </div>
-                    <h1 className="home-title">Spring Boot React OAuth2 Social Login Demo</h1>
+                    <b className="home-subtitle">America loses up to 218 BILLION
+                        dollars from wasted food</b>
+                    <br/>
+                    <b className="home-subtitle">Restaurants in the United
+                        States are wasting 22 to 33 BILLION pounds of food a
+                        year</b>
+                    <b className="home-ref">[1]</b>
+                    <br/>
+                    <h2 className="home-subtitle">Let's change that!</h2>
+                    <h1 className="home-title">86 No More: A Kitchen Intelligence System</h1>
+                    <b className="home-ref">[1] Gunders, Dana. “Wasted: How America
+                        is Losing Up to 40 Percent of Its Food from Farm to Fork
+                        to Landfill.” Natural Resources Defense Council, 2017.
+                        Retrieved September 27, 2021, from https://www.nrdc.org
+                        /sites/default/files/wasted-2017-report.pdf</b>
                 </div>
             </div>
         )
