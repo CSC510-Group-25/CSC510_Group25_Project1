a099e5916935bc9c4f9ee0b1f3066253439024d1
shraddhamishra7 <shraddha.mishra95@gmail.com>
2021-09-29 23:19:20 -0400

    updated value for graph

--- react-social/src/components/LineChart.js
+++ react-social/src/components/LineChart.js
@@ -18,24 +18,24 @@ export default function App() {
 }
 
 const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
     datasets: [
         {
-            label: "First dataset",
-            data: [33, 53, 85, 41, 44, 65],
+            label: "Number of orders per month",
+            data: [33, 69, 80, 111, 128, 90, 60, 40, 70, 94, 111, 89],
             fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)"
+            backgroundColor: "rgba(92,75,152,0.2)",
+            borderColor: "rgba(92,75,152,1)"
         }
     ]
 };
 
 const data1 = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
     datasets: [
         {
-            label: "First dataset",
-            data: [33, 53, 85, 41, 44, 65],
+            label: "Total Sales per month",
+            data: [3300, 5500, 8700, 10100, 10800, 8000, 7670, 5890, 4990, 8400, 11100, 8900],
             fill: true,
             backgroundColor: "rgba(75,192,192,0.2)",
             borderColor: "rgba(75,192,192,1)"
@@ -44,14 +44,14 @@ const data1 = {
 };
 
 const data2 = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
     datasets: [
         {
-            label: "First dataset",
-            data: [33, 53, 85, 41, 44, 65],
+            label: "Quantity of material wasted",
+            data: [906, 793, 531, 799, 710, 608, 559, 510, 488, 430, 370, 350],
             fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)"
+            backgroundColor: "rgba(0,55,55,0.2)",
+            borderColor: "rgba(0,55,55,1)"
         }
     ]
 };
