9c4eb7c0f1d5021ba04d36651823a83203c510a3
shraddhamishra7 <shraddha.mishra95@gmail.com>
2021-09-29 22:59:27 -0400

    multiple graphs

--- react-social/src/components/LineChart.js
+++ react-social/src/components/LineChart.js
@@ -4,7 +4,15 @@ import { Line } from "react-chartjs-2";
 export default function App() {
     return (
         <div className="App">
-            <Line data={data} />
+            <Line data={data}
+                  height={50}
+                  options={{ maintainAspectRatio: true }}/>
+            <Line data={data1}
+                  height={50}
+                  options={{ maintainAspectRatio: true }}/>
+            <Line data={data2}
+                  height={50}
+                  options={{ maintainAspectRatio: true }}/>
         </div>
     );
 }
@@ -18,12 +26,32 @@ const data = {
             fill: true,
             backgroundColor: "rgba(75,192,192,0.2)",
             borderColor: "rgba(75,192,192,1)"
-        },
+        }
+    ]
+};
+
+const data1 = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
         {
-            label: "Second dataset",
-            data: [33, 25, 35, 51, 54, 76],
-            fill: false,
-            borderColor: "#742774"
+            label: "First dataset",
+            data: [33, 53, 85, 41, 44, 65],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)"
+        }
+    ]
+};
+
+const data2 = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+        {
+            label: "First dataset",
+            data: [33, 53, 85, 41, 44, 65],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)"
         }
     ]
 };
