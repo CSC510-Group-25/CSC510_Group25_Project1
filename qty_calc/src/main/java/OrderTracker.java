507207fa302500951e1ca1871f80e86069f515b4
Leila Moran <89357283+snapcat@users.noreply.github.com>
2021-09-30 21:22:08 -0400

    added license

--- qty_calc/src/main/java/OrderTracker.java
+++ qty_calc/src/main/java/OrderTracker.java
@@ -6,6 +6,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021. CSC510-Group-25
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**************************
+ * Originally authored by: Leila Moran
+ * GitHub ID: snapcat
+ *
+ * CSC510 Fall 2021
+ * North Carolina State University
+ *
+ * File name: OrderTracker.java
+ **************************/
+
 /**
  * OrderTracker class.<br>
  *
