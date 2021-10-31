634838dec9cf82116aa0872c5b94ba8b354f23b0
dgupta23 <dgupta23@ncsu.edu>
2021-10-30 22:21:12 -0400

    Added Update controller

--- react-social/src/user/profile/Profile.js
+++ react-social/src/user/profile/Profile.js
@@ -1,52 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Profile.css';
 import Alert from 'react-s-alert';
+import {updateProfile} from "../../util/APIUtils";
 
 class Profile extends Component {
     constructor(props) {
         super(props);
         console.log(props);
         this.state = {
-                    restaurantName: '',
-                    name: '',
-                    email: '',
-                    password: '',
-                    role: ''
-                }
-                this.handleInputChange = this.handleInputChange.bind(this);
-                this.handleSubmit = this.handleSubmit.bind(this);
+            restaurantName: '',
+            name: '',
+            email: '',
+            role: ''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
     }
-     handleInputChange(event) {
-            const target = event.target;
-            const inputName = target.name;
-            const inputValue = target.value;
 
-            this.setState({
-                [inputName] : inputValue
-            });
-        }
+    handleInputChange(event) {
+        const target = event.target;
+        const inputName = target.name;
+        const inputValue = target.value;
 
-     handleSubmit(event) {
-            event.preventDefault();
+        this.setState({
+            [inputName]: inputValue
+        });
+    }
 
-            const Profilechange = Object.assign({}, this.state);
+    handleSubmit(event) {
+        event.preventDefault();
 
-            Profile(Profilechange)
+        const updateProfileRequest = Object.assign({}, this.state);
+
+        updateProfile(updateProfileRequest)
             .then(response => {
-                Alert.success("You're successfully registered. Please login to continue!");
+                Alert.success("You've successfully updated your profile.");
                 this.props.history.push("/login");
             }).catch(error => {
-                Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
-            });
-       }
+            Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
+        });
+    }
+
     render() {
         return (
             <div className="profile-container">
                 <div className="container">
                     <div className="profile-info">
                         <div className="profile-avatar">
-                            { 
+                            {
                                 this.props.currentUser.imageUrl ? (
                                     <img src={this.props.currentUser.imageUrl} alt={this.props.currentUser.name}/>
                                 ) : (
@@ -58,44 +60,39 @@ class Profile extends Component {
                             }
                         </div>
                         <div className="profile-name">
-                          <h2>{this.props.currentUser.name}</h2>
+                            <h2>{this.props.currentUser.name}</h2>
 
-                          <form onSubmit={this.handleSubmit}>
-                                          <div className="form-item">
-                                              <input type="text" name="restaurantName"
-                                                  className="form-control" placeholder={this.props.currentUser.restaurantName}
-                                                  value={this.state.restaurantName} onChange={this.handleInputChange} required/>
-                                          </div>
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-item">
+                                    <input type="text" name="restaurantName"
+                                           className="form-control" placeholder={this.props.currentUser.restaurantName}
+                                           value={this.state.restaurantName} onChange={this.handleInputChange}
+                                           required/>
+                                </div>
 
-                                          <div className="form-item">
-                                              <input type="text" name="name"
-                                                  className="form-control" placeholder={this.props.currentUser.name}
-                                                  value={this.state.name} onChange={this.handleInputChange} required/>
-                                          </div>
-
-
-                                          <div className="form-item">
-                                              <input type="text" name="role"
-                                                  className="form-control" placeholder={this.props.currentUser.role}
-                                                  value={this.state.role}  onChange={this.handleInputChange} required/>
-                                          </div>
-                                          <div className="form-item">
-                                              <button type="submit" className="btn btn-block btn-primary" >Update</button>
-                                          </div>
-                                      </form>
+                                <div className="form-item">
+                                    <input type="text" name="name"
+                                           className="form-control" placeholder={this.props.currentUser.name}
+                                           value={this.state.name} onChange={this.handleInputChange} required/>
+                                </div>
 
 
-
-
+                                <div className="form-item">
+                                    <input type="text" name="role"
+                                           className="form-control" placeholder={this.props.currentUser.role}
+                                           value={this.state.role} onChange={this.handleInputChange} required/>
+                                </div>
+                                <div className="form-item">
+                                    <button type="submit" className="btn btn-block btn-primary">Update</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </div>    
+                </div>
             </div>
         );
     }
 }
-
-
 
 
 export default Profile
--- spring-social/src/main/java/com/example/springsocial/controller/UserController.java
+++ spring-social/src/main/java/com/example/springsocial/controller/UserController.java
@@ -2,17 +2,22 @@ package com.example.springsocial.controller;
 
 import com.example.springsocial.exception.ResourceNotFoundException;
 import com.example.springsocial.model.User;
+import com.example.springsocial.payload.ApiResponse;
+import com.example.springsocial.payload.UpdateRequest;
 import com.example.springsocial.repository.UserRepository;
 import com.example.springsocial.security.CurrentUser;
 import com.example.springsocial.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 public class UserController {
@@ -28,7 +33,20 @@ public class UserController {
     }
 
     @PostMapping("/update")
-    public User updateCurrentuser(@Valid ResponseBody User user){
+    public ResponseEntity<?> updateCurrentuser(@Valid @RequestBody UpdateRequest request){
+        User user = new User();
+        user.setName(request.getName());
 
+        user.setEmail(request.getEmail());
+        user.setRestaurantName(request.getRestaurantName());
+        user.setRole(request.getRole());
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/user/me")
+                .buildAndExpand(result.getId()).toUri();
+        logger.info("User updated successfully");
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "User registered successfully@"));
     }
 }
