5a7f83025decc132ce3334bbd780d853b90d8917
Het <het.thakkar809@gmail.com>
2021-10-31 17:06:51 -0400

    add: notification related queries to inventory repository

--- spring-social/src/main/java/com/example/springsocial/repository/InventoryRepository.java
+++ spring-social/src/main/java/com/example/springsocial/repository/InventoryRepository.java
@@ -5,17 +5,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 /**
-* The inventory repository is a mechanism for encapsulating storage, retrieval, and search behavior which emulates
-* a collection of objects.
-*/
-public interface InventoryRepository extends JpaRepository<Inventory, String>{
+ * The inventory repository is a mechanism for encapsulating storage, retrieval, and search behavior which emulates
+ * a collection of objects.
+ */
+public interface InventoryRepository extends JpaRepository<Inventory, String> {
 
     List<Inventory> findAll();
+
+    List<Inventory> findInventoryByRestaurantID(String restaurantId);
+
+    List<Inventory> findInventoryByRestaurantNameAndDateExpiredBetween(String restaurant_name, Date expiredStart, Date expiredEnd);
+
+    List<Inventory> findInventoryByRestaurantNameAndDateExpiredLessThan(String restaurant_name, Date expiredDate);
+
+    List<Inventory> findInventoryByRestaurantNameAndBatchQtyLessThanEqual(String restaurant_name, int maxQuantity);
 
 }//    Optional<Inventory> findByRestaurant(String restaurantID);
 
